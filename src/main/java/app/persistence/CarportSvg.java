13cc157b20ab91223a13cb503a2a753639697986
Mikkel-ao <mikkel.ao.dev@gmail.com>
2025-05-06 11:27:09 +0200

    Added methods for placing posts and displaying spacing measurements

--- src/main/java/app/persistence/CarportSvg.java
+++ src/main/java/app/persistence/CarportSvg.java
@@ -6,7 +6,9 @@ public class CarportSvg {
     private int width;
     private int length;
     private Svg carportSvg;
+    private String style = "stroke-width:1px; stroke:#000000; fill: #ffffff";
     private double rafterWidth = 4.5;
+    private double postWidth = 9.7;
 
 
     public CarportSvg(int width, int length) {
@@ -21,41 +23,67 @@ public class CarportSvg {
         drawBase();
         addBeams();
         addRafters();
+        addPosts();
         addDimensions();
     }
 
     private void drawBase() {
-        carportSvg.addRectangle(0, 0, width, length, "stroke-width:1px; stroke:#000000; fill: #ffffff");
+        carportSvg.addRectangle(0, 0, width, length, style);
     }
+
     // Rem
     private void addBeams() {
-        String beamStyle = "stroke-width:1px; stroke:#000000; fill: #ffffff";
-        carportSvg.addRectangle(0, 35, 4.5, length, beamStyle);
-        carportSvg.addRectangle(0, width-35, 4.5, length, beamStyle);
+        carportSvg.addRectangle(0, 35, rafterWidth, length, style);
+        carportSvg.addRectangle(0, width-35, rafterWidth, length, style);
     }
+
     // Spær
     private void addRafters() {
-        String rafterStyle = "stroke:#000000; fill: #ffffff";
         int rafterCount = Calculator.calcAmountOfRafters(length);
         double spacing = Calculator.getRafterSpacing(length);
 
         for (int i = 0; i < rafterCount; i++) {
             double x = i * (spacing + rafterWidth);
-            carportSvg.addRectangle(x, 0.0, width, rafterWidth, rafterStyle);
+            carportSvg.addRectangle(x, 0.0, width, rafterWidth, style);
+
+            if (i > 0) {
+                double prevRafter = (i - 1) * (spacing + rafterWidth);
+                double start = prevRafter + rafterWidth;
+                double end = x;
+                double mid = (start + end) / 2;
+                carportSvg.addArrow((int) start, width - 20, (int) end, width - 20, style);
+                // -17 to center the text with the arrow for now. String.format for 2 decimals
+                carportSvg.addText((int) mid-17, (width - 20) + 15, 0, String.format("%.2f", end - start));
+            }
+
+        }
+    }
+
+    // Stolpe
+    private void addPosts() {
+        int postCount = Calculator.calcAmountOfPoles(length);
+        // TODO: Use method call for correct spacing
+        double spacing = 250;
+
+        for (int i = 0; i < postCount; i++) {
+            double x = i * (spacing + postWidth);
+            // y:'s value centers the post with the beam.
+            carportSvg.addRectangle(x, 33, postWidth, postWidth, style); // Upper
+            carportSvg.addRectangle(x, width - 37, postWidth, postWidth, style); // Lower
         }
     }
 
     private void addDimensions() {
         // Horizontal dimension of the drawing, length of the carport
-        carportSvg.addArrow(0, width+10, length, width+10, "stroke:black; stroke-width:1");
+        carportSvg.addArrow(0, width+10, length, width+10, style);
         carportSvg.addText(length/2, width+25, 0, length+" cm");
 
         // Vertical dimension of the drawing, width of the carport
-        carportSvg.addArrow(length+40, 0, length+40, width, "stroke:black; stroke-width:1");
+        carportSvg.addArrow(length+40, 0, length+40, width, style);
         carportSvg.addText(length+45, width/2, 90, width+" cm");
 
         // Inner vertical dimension
-        carportSvg.addArrow(length+20, 35, length+20, width-35, "stroke:black; stroke-width:1");
+        carportSvg.addArrow(length+20, 35, length+20, width-35, style);
         carportSvg.addText(length+25, width/2, 90, width-70+" cm");
 
     }
