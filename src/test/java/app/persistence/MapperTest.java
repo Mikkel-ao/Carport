d0b36cf96110ee863a4410ad495e9361a06129ae
Phils1993 <phils1993@outlook.com>
2025-05-27 14:15:04 +0200

    fat jar

--- src/test/java/app/persistence/MapperTest.java
+++ src/test/java/app/persistence/MapperTest.java
@@ -14,8 +14,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MapperTest {
-
-    private static ConnectionPool testConnectionPool;
+private static ConnectionPool testConnectionPool;
     private static int insertedOrderId;
 
 
